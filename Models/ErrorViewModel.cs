634574b8726555407661d76b885f7aaf7425bb6d
UongHoangAnh45 <88814845+BanhMat45@users.noreply.github.com>
2022-03-28 07:15:13 +0700

    First_commit_UHA033_2022

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UongHoangAnh2022033.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
